562db1f4ffbedb9e07f323cc0b5ecdf5f9f1c50f
Alan Eugenio <alan.349@hotmail.com>
2023-07-24 10:15:38 -0300

    Fixed Admin Content Nav when there was no content

--- Modules/Content/Resources/views/components/admin/nav-item.blade.php
+++ Modules/Content/Resources/views/components/admin/nav-item.blade.php
@@ -11,28 +11,31 @@
         </x-admin.nav-dropdown-link>
     </x-slot>
     <x-slot name="subContent">
-        @foreach (config('contentNavs') as $slug => $nav)
-            @if ($nav->status->ativo())
-                @if ($nav->type->multiple())
-                    <x-admin.nav-dropdown :sub="true" :active="in_array($nav->id, [request()->nav?->id, request()->content?->nav->id])">
-                        <x-slot name="trigger">
+        @if (config('contentNavs'))
+            @foreach (config('contentNavs') as $slug => $nav)
+                @if ($nav->status->ativo())
+                    @if ($nav->type->multiple())
+                        <x-admin.nav-dropdown :sub="true" :active="in_array($nav->id, [request()->nav?->id, request()->content?->nav->id])">
+                            <x-slot name="trigger">
+                                {{ $nav->title }}
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-admin.nav-dropdown-link :href="route('admin.contents.create', $nav->id)" :active="request()->routeIs('admin.contents.create', $nav->id)">
+                                    Cadastrar
+                                </x-admin.nav-dropdown-link>
+                                <x-admin.nav-dropdown-link :href="route('admin.contents.index', $nav->id)" :active="request()->routeIs('admin.contents.index') &&
+                                    request()->nav?->id == $nav->id">
+                                    Listar
+                                </x-admin.nav-dropdown-link>
+                            </x-slot>
+                        </x-admin.nav-dropdown>
+                    @else
+                        <x-admin.nav-dropdown-link :sub="true" :href="route('admin.contents.index', $nav->id)" :active="in_array($nav->id, [request()->nav?->id, request()->content?->nav->id])">
                             {{ $nav->title }}
-                        </x-slot>
-                        <x-slot name="content">
-                            <x-admin.nav-dropdown-link :href="route('admin.contents.create', $nav->id)" :active="request()->routeIs('admin.contents.create', $nav->id)">
-                                Cadastrar
-                            </x-admin.nav-dropdown-link>
-                            <x-admin.nav-dropdown-link :href="route('admin.contents.index', $nav->id)" :active="request()->routeIs('admin.contents.index') && request()->nav?->id == $nav->id">
-                                Listar
-                            </x-admin.nav-dropdown-link>
-                        </x-slot>
-                    </x-admin.nav-dropdown>
-                @else
-                    <x-admin.nav-dropdown-link :sub="true" :href="route('admin.contents.index', $nav->id)" :active="in_array($nav->id, [request()->nav?->id, request()->content?->nav->id])">
-                        {{ $nav->title }}
-                    </x-admin.nav-dropdown-link>
+                        </x-admin.nav-dropdown-link>
+                    @endif
                 @endif
-            @endif
-        @endforeach
+            @endforeach
+        @endif
     </x-slot>
 </x-admin.nav-dropdown>
