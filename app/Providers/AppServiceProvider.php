1308fe80c2fe23a86525e23934ace94c7ca64f37
Alan CGDW <alan@cgdw.com.br>
2023-08-16 17:19:48 -0300

    Added validation to check if product and content modules are present/enabled

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\Define;
 use App\Models\Integration;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
-use Modules\Content\Entities\ContentNav;
 
 class AppServiceProvider extends ServiceProvider {
     /**
@@ -24,13 +23,6 @@ class AppServiceProvider extends ServiceProvider {
         if (!app()->runningInConsole()) {
             Config::set('defines', Define::first());
 
-            $navs = ContentNav::orderBy('type')->get()->mapWithKeys(function ($item) {
-                return [$item['slug'] => $item];
-            });
-            foreach ($navs as $nav => $id) {
-                Config::set('contentNavs.' . $nav, $id);
-            }
-
             $integrations = Integration::all()->mapWithKeys(function ($item) {
                 return [$item['integration'] => $item];
             });
@@ -40,6 +32,19 @@ class AppServiceProvider extends ServiceProvider {
                     ['id' => $item['id'], 'status' => $item['status'], 'defines' => json_decode($item['defines'], true), 'editable' => $item['editable']]
                 );
             }
+
+            $modules = json_decode(file_get_contents('../modules_statuses.json'));
+            $contentModule = isset($modules->Content) && $modules->Content == true;
+
+            if ($contentModule) {
+                $navs = \Modules\Content\Entities\ContentNav::orderBy('type')->get()->mapWithKeys(function ($item) {
+                    return [$item['slug'] => $item];
+                });
+                foreach ($navs as $nav => $id) {
+                    Config::set('contentNavs.' . $nav, $id);
+                }
+            }
+
         }
     }
 }
